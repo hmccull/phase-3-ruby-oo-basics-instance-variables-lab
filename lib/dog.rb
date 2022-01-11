640ac950934aff4dce29c37c2327b1e9cbb115f6
Hannah McCullough <hmac091@gmail.com>
2022-01-11 09:47:50 -0700

    commit tree 850f676e2bb80fe5be1d2b36334cde15ca0650ee

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    #  setter method
+    def name=(dog_name)
+        # assign the @this_dogs_name instance variable a value of whatever is passed as an argument
+        @this_dogs_name = dog_name
+    end
 
+    # getter method
+    def name
+        # access the data from the @this_dogs_name instance variableand return it
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+# call the Dog#name= method with a value of "Lassie"
+# equivalent to lassie.name = "Lassie"
+lassie.name = "Lassie"
+# call the Dog#name method
+lassie.name
